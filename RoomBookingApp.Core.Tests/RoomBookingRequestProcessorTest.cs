164ab38ea85f64b0cd5b1a842f2a26ba3227da4c
Adrian Moa Montores <adrian_moa@hotmail.com>
2025-04-21 16:11:41 -0600

    Throw Exception when request is null - Processor

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,12 +1,18 @@
 ﻿using RoomBookingApp.Core.Models;
 using RoomBookingApp.Core.Processors;
 using Shouldly;
-using System.ComponentModel.DataAnnotations;
 
 namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
     {
+        private readonly RoomBookingRequestProcessor _processor;
+
+        public RoomBookingRequestProcessorTest()
+        {
+            _processor = new RoomBookingRequestProcessor();
+        }
+
         [Fact]
         public void Should_Return_Room_Booking_Response_Response_With_Request_Values()
         {
@@ -18,21 +24,21 @@ namespace RoomBookingApp.Core
                 Date = DateTime.Now,
             };
 
-            var processor = new RoomBookingRequestProcessor();
-
             //act
-            RoomBookingResult result = processor.BookRoom(request);
+            RoomBookingResult result = _processor.BookRoom(request);
 
             //assert
-            //Assert.NotNull(result);
-            //Assert.Equal(request.FullName, result.FullName);
-            //Assert.Equal(request.Email, result.Email);
-            //Assert.Equal(request.Date, result.Date);
-
             result.ShouldNotBeNull();
             result.FullName.ShouldBe(request.FullName);
             result.Email.ShouldBe(request.Email);
             result.Date.ShouldBe(request.Date);
+        }
+
+        [Fact]
+        public void Should_Throw_Exception_For_Null_Request()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() => _processor.BookRoom(null));
+            exception.ParamName.ShouldBe("bookingRequest");
         }
     }
 }
